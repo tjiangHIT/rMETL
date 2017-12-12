9601c91fcd9f36a59111de4f51caee6d574162d5
tjiang <tjiang@hit.edu.cn>
2017-12-12 03:42:45 -0600

    filter abnormal reads

--- filter_reads/abnormal_filtering.py
+++ filter_reads/abnormal_filtering.py
@@ -1,9 +1,23 @@
 import sys
 
+def acquire_clip(cigar, flag):
+	contig_num = len(cigar.split(flag))
+	if contig_num == 1:
+		return 0
+	if contig_num == 2:
+		if cigar[-1] == flag:
+			return int(cigar.split(flag)[0].split('M')[-1].split('D')[-1].split('I')[-1])
+		else:
+			return int(cigar.split(flag)[0])
+	if contig_num == 3:
+		first = int(cigar.split(flag)[0])
+		last = int(cigar.split(flag)[1].split('M')[-1].split('D')[-1].split('I')[-1])
+		return first + last
+
 def analysis_read(path):
-	unaligned_num = 0
-	aligned_num = 0
-	dic = {}
+	unaligned_dic = {}
+	aligned_dic = {}
+	# dic = {}
 	file = open(path)
 	for line in file:
 		seq = line.strip()
@@ -13,15 +27,39 @@ def analysis_read(path):
 			seq = seq.split('\t')
 			cigar = seq[5]
 			read_id = seq[0]
+			read_len = int(len(seq[9]))
+
 			if cigar[0] == '*':
-				unaligned_num += 1
-				if read_id not in dic:
-					dic[read_id] = 1
+				# unaligned_num += 1
+				if read_id not in unaligned_dic:
+					unaligned_dic[read_id] = 1
 			else:
-				aligned_num += 1
+				soft_clip = acquire_clip(cigar, 'S')
+				hard_clip = acquire_clip(cigar, 'H')
+				clip = soft_clip + hard_clip
+				if clip >= read_len * 0.1:
+					# print read_id
+					# unaligned_num += 1
+					if read_id not in unaligned_dic:
+						unaligned_dic[read_id] = 1
+				else:
+					if read_id not in aligned_dic:
+						aligned_dic[read_id] = 1
+					# aligned_num += 1
+				# aligned_num += 1
 	file.close()
-	print unaligned_num, aligned_num
-	return dic
+	# print unaligned_num, aligned_num
+
+	del_read_id = []
+	for key in unaligned_dic:
+		if key in aligned_dic:
+			del_read_id.append(key)
+			# unaligned_dic.pop(key)
+	for i in xrange(len(del_read_id)):
+		unaligned_dic.pop(del_read_id[i])
+
+	# print len(unaligned_dic), len(aligned_dic), len(unaligned_dic)+len(aligned_dic)
+	return unaligned_dic
 
 def main():
 	unaligned_read_id_minimap2 = analysis_read(sys.argv[1])
