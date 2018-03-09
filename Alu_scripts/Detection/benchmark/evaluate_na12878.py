aca8ec290a4ad603c184fffecdeea81fa17bd4cd
tjiang <tjiang@hit.edu.cn>
2018-03-09 20:49:26 +0800

    adjust

--- Alu_scripts/Detection/benchmark/evaluate_na12878.py
+++ Alu_scripts/Detection/benchmark/evaluate_na12878.py
@@ -6,9 +6,9 @@ from data_collection import *
 # dic_tangram = dict()
 # dic_retroseq = dict()
 DICT = list()
-standard = 50
+standard = 10
 
-def check_locus(chr, pos):
+def check_locus(chr, pos, flag):
 	dic_tea = DICT[0]
 	dic_tangram = DICT[1]
 	dic_retroseq = DICT[2]
@@ -17,7 +17,7 @@ def check_locus(chr, pos):
 		for i in xrange(len(dic_tea[chr])):
 			truth_pos = dic_tea[chr][i][0]
 			if truth_pos - standard <= pos and pos <= truth_pos + standard:
-				dic_tea[chr][i][3] = 1
+				dic_tea[chr][i][3] = flag
 				result[0] = 1
 				# break
 	# for key in dic_tangram:
@@ -28,37 +28,37 @@ def check_locus(chr, pos):
 			truth_pos = dic_tangram[chr][i][0]
 			# print truth_pos
 			if truth_pos - standard <= pos and pos <= truth_pos + standard:
-				dic_tangram[chr][i][3] = 1
+				dic_tangram[chr][i][3] = flag
 				result[1] = 1
 				# break
 	if chr in dic_retroseq:
 		for i in xrange(len(dic_retroseq[chr])):
 			truth_pos = dic_retroseq[chr][i][0]
 			if truth_pos - standard <= pos and pos <= truth_pos + standard:
-				dic_retroseq[chr][i][3] = 1
+				dic_retroseq[chr][i][3] = flag
 				result[2] = 1
 				# break
 	return result
 
-def call_overlap(local_l, key1, key2):
+def call_overlap(local_l, key1, key2, flag):
 	chr = local_l[0]
 	pos = local_l[1]
 	overlap = 0
 	if chr in DICT[key1]:
 		for i in xrange(len(DICT[key1][chr])):
-			if DICT[key1][chr][i][3] == 0:
+			if DICT[key1][chr][i][3] != flag:
 				local_pos = DICT[key1][chr][i][0]
 				if local_pos - standard <= pos and pos <= local_pos + standard:
 					overlap += 1
 	if chr in DICT[key2]:
 		for i in xrange(len(DICT[key2][chr])):
-			if DICT[key2][chr][i][3] == 0:
+			if DICT[key2][chr][i][3] != flag:
 				local_pos = DICT[key2][chr][i][0]
 				if local_pos - standard <= pos and pos <= local_pos + standard:
 					overlap += 1
 	return overlap
 
-def evaluation(p, op):
+def evaluation(p, op, p2):
 	# r = check_locus('1', 5934294)
 	# print r
 	nice_job_1 = 0
@@ -77,7 +77,7 @@ def evaluation(p, op):
 			temp_chr = chr
 			# print("[INFO]: Testing chromsome %s."%(temp_chr))
 		# print chr, pos
-		result = check_locus(chr, pos)
+		result = check_locus(chr, pos, 1)
 		if sum(result) == 1:
 			nice_job_1 += 1
 		if sum(result) == 2:
@@ -94,7 +94,7 @@ def evaluation(p, op):
 		for i in xrange(len(DICT[0][key])):
 			if DICT[0][key][i][3] == 0:
 				Np += 1
-				overlap = call_overlap(DICT[0][key][i], 1, 2)
+				overlap = call_overlap(DICT[0][key][i], 1, 2, 1)
 				out_file.write("Tea\t%s\t%d\t%s\t%d\n"%(key, DICT[0][key][i][0], DICT[0][key][i][2], overlap))
 			else:
 				Tp += 1
@@ -106,7 +106,7 @@ def evaluation(p, op):
 		for i in xrange(len(DICT[1][key])):
 			if DICT[1][key][i][3] == 0:
 				Np += 1
-				overlap = call_overlap(DICT[1][key][i], 0, 2)
+				overlap = call_overlap(DICT[1][key][i], 0, 2, 1)
 				out_file.write("Tangram\t%s\t%d\t%s\t%d\n"%(key, DICT[1][key][i][0], DICT[1][key][i][2], overlap))
 			else:
 				Tp += 1
@@ -118,12 +118,70 @@ def evaluation(p, op):
 		for i in xrange(len(DICT[2][key])):
 			if DICT[2][key][i][3] == 0:
 				Np += 1
-				overlap = call_overlap(DICT[2][key][i], 0, 1)
+				overlap = call_overlap(DICT[2][key][i], 0, 1, 1)
 				out_file.write("RetroSeq\t%s\t%d\t%s\t%d\n"%(key, DICT[2][key][i][0], DICT[2][key][i][2], overlap))
 			else:
 				Tp += 1
 	print("[INFO]: For RetroSeq callset: Y for %d, N for %d(%0.5f)."%(Tp, Np, Tp*1.0/(Tp+Np)))
 	out_file.close()
+
+	nice_job_1 = 0
+	nice_job_2 = 0
+	nice_job_3 = 0
+	file = open(p2, 'r')
+	for line in file:
+		seq = line.strip('\n').split('\t')
+		if seq[0][0] == '#':
+			continue
+		chr = seq[0]
+		subtype = seq[10]
+		start_pos = int(seq[13])
+		# end_pos = int(seq[15])
+		if subtype == 'INS' or subtype == "DUP":
+			result = check_locus(chr, start_pos, 2)
+			if sum(result) == 1:
+				nice_job_1 += 1
+			if sum(result) == 2:
+				nice_job_2 +=1
+			if sum(result) == 3:
+				nice_job_3 +=1
+	file.close()
+	print("[INFO]: (1,2,3) %d,%d,%d signal locuses Sniffles are good!"%(nice_job_1, nice_job_2, nice_job_3))
+	Tp = 0
+	Np = 0
+	for key in DICT[0]:
+		for i in xrange(len(DICT[0][key])):
+			if DICT[0][key][i][3] != 2:
+				Np += 1
+				overlap = call_overlap(DICT[0][key][i], 1, 2, 2)
+				# out_file.write("Tea\t%s\t%d\t%s\t%d\n"%(key, DICT[0][key][i][0], DICT[0][key][i][2], overlap))
+			else:
+				Tp += 1
+	print("[INFO]: For Tea callset: Y for %d, N for %d(%0.5f)."%(Tp, Np, Tp*1.0/(Tp+Np)))
+
+	Tp = 0
+	Np = 0
+	for key in DICT[1]:
+		for i in xrange(len(DICT[1][key])):
+			if DICT[1][key][i][3] != 2:
+				Np += 1
+				overlap = call_overlap(DICT[1][key][i], 0, 2, 2)
+				# out_file.write("Tangram\t%s\t%d\t%s\t%d\n"%(key, DICT[1][key][i][0], DICT[1][key][i][2], overlap))
+			else:
+				Tp += 1
+	print("[INFO]: For Tangram callset: Y for %d, N for %d(%0.5f)."%(Tp, Np, Tp*1.0/(Tp+Np)))
+
+	Tp = 0
+	Np = 0
+	for key in DICT[2]:
+		for i in xrange(len(DICT[2][key])):
+			if DICT[2][key][i][3] != 2:
+				Np += 1
+				overlap = call_overlap(DICT[2][key][i], 0, 1, 2)
+				# out_file.write("RetroSeq\t%s\t%d\t%s\t%d\n"%(key, DICT[2][key][i][0], DICT[2][key][i][2], overlap))
+			else:
+				Tp += 1
+	print("[INFO]: For RetroSeq callset: Y for %d, N for %d(%0.5f)."%(Tp, Np, Tp*1.0/(Tp+Np)))
 
 
 def main():
@@ -149,9 +207,11 @@ def main():
 
 	signal_path = sys.argv[5]
 	out_path = sys.argv[6]
+	sniffles_path = sys.argv[7]
 	print("[INFO]: The path of the Signal file: %s"%(signal_path))
+	print("[INFO]: The path of the Sniffles file: %s"%(sniffles_path))
 	print("[INFO]: Starting to evaluate...")
-	evaluation(signal_path, out_path)
+	evaluation(signal_path, out_path, sniffles_path)
 
 	print("[INFO]: Finished in %0.2f seconds."%(time.time() - starttime))
 	print("[INFO]: Have a nice day! ^.^ ")
