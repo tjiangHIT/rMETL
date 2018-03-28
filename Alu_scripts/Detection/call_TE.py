75926bf1dde004aff75ca8201237f6fdb69afdbf
tjiang <tjiang@hit.edu.cn>
2018-03-28 10:29:16 +0800

    optimize the subtype calling

--- Alu_scripts/Detection/call_TE.py
+++ Alu_scripts/Detection/call_TE.py
@@ -52,6 +52,44 @@ def load_sam(p1):
 		# print("%s\t%s\t%s\t%s"%(i[0], breakpoint, insert_size, final_type))
 		print "\t".join(i)
 
+def load_sam_2(p1):
+	# samfile = pysam.AlignmentFile(p1)
+	AlignmentFile = open(p1, 'r')
+	for line in AlignmentFile:
+		seq = line.strip('\n').split('\t')
+		if seq[0][0] == '@':
+			continue
+		chr, breakpoint, insert_size = parse_name(seq[0])
+		Flag = int(seq[1])
+		sub_type = seq[2]
+		MAPQ = int(seq[4])
+		if flag_dic[Flag] != 0 and int(insert_size) >= 50:
+			# to do something
+			key = "%s_%s_%s"%(chr, breakpoint, insert_size)
+			if key not in cluster_dic:
+				cluster_dic[key] = [MAPQ, sub_type]
+			else:
+				if MAPQ > cluster_dic[key][0]:
+					cluster_dic[key] = [MAPQ, sub_type]
+			# cluster_dic[key].append(sub_type+"&"+str(flag_dic[Flag]))
+			# cluster_dic[key].append(sub_type)
+	AlignmentFile.close()
+
+	sort_list = list()
+	for i in cluster_dic:
+		chr, breakpoint, insert_size = parse_name(i)
+		# final = acquire_count_max(cluster_dic[i])
+		final_type = cluster_dic[i][1]
+		# final_strand = final.split('&')[1]
+		# final_strand = acquire_count_max(cluster_dic[i][1])
+		# sort_list.append([chr, breakpoint, insert_size, final_type, strand_dic[int(final_strand)]])
+		sort_list.append([chr, breakpoint, insert_size, final_type])
+		# print("%s\t%s\t%s\t%s"%(chr, breakpoint, insert_size, final_type))
+	sort_list = sorted(sort_list, key = lambda x:(x[0], int(x[1])))
+	for i in sort_list:
+		# print("%s\t%s\t%s\t%s"%(i[0], breakpoint, insert_size, final_type))
+		print "\t".join(i)
+
 if __name__ == '__main__':
 	path = sys.argv[1]
-	load_sam(path)
+	load_sam_2(path)
