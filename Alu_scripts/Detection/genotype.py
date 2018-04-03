7c76c97521de785dc2af337601abd8a0a7f454e8
tjiang <tjiang@hit.edu.cn>
2018-04-03 15:41:31 +0800

    add genotype likelyhood

--- Alu_scripts/Detection/genotype.py
+++ Alu_scripts/Detection/genotype.py
@@ -1,10 +1,14 @@
 
 from scipy.special import comb
+# from math import log10
+import numpy as np
 
 
 P_homozygous_ref = 0.02
 P_heterozygous = 0.5
 P_homozygous_ME = 0.98
+
+GL_TAG = ['0/0', '0/1', '1/1']
 
 def caculate_genotype_likelyhood(Nalt, Ntotal):
 	ref_ref = 1.0 / 3
@@ -15,10 +19,20 @@ def caculate_genotype_likelyhood(Nalt, Ntotal):
 	P_het = ref_ME * comb(Ntotal, Nalt) * P_heterozygous ** Nalt * (1 - P_heterozygous) ** (Ntotal - Nalt)
 	P_hom = ME_ME * comb(Ntotal, Nalt) * P_homozygous_ME ** Nalt * (1 - P_homozygous_ME) ** (Ntotal - Nalt)
 	P_total = P_ref + P_het + P_hom
+	# P_ref = -10 * log10(P_ref / P_total)
+	# P_het = -10 * log10(P_het / P_total)
+	# P_hom = -10 * log10(P_hom / P_total)
 	P_ref = P_ref / P_total
 	P_het = P_het / P_total
 	P_hom = P_hom / P_total
-	print P_ref, P_het, P_hom
+	genotype_likelyhood = [P_ref, P_het, P_hom]
+	final_gl = max(genotype_likelyhood)
+	tag = genotype_likelyhood.index(final_gl) 
+	# P_ref = np.floor(-10 * np.log10(P_ref / P_total))
+	# P_het = np.floor(-10 * np.log10(P_het / P_total))
+	# P_hom = np.floor(-10 * np.log10(P_hom / P_total))
+	# np.floor(-10 * np.log10(np.power(2, discordant) / np.power(2, expected_discordant_lower_bound)))
+	return GL_TAG[tag], final_gl
 
-if __name__ == '__main__':
-	caculate_genotype_likelyhood(10, 40)
+# if __name__ == '__main__':
+# 	caculate_genotype_likelyhood(20, 24)
